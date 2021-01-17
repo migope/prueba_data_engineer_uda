7d53a65550a8755ceabd202daca0ee5b7038fcc0
migope <migue3080@gmail.com>
2021-01-17 20:11:10 +0100

    Median and p95

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import argparse
 import logging
 import json
-from datetime import time
+from datetime import time, timedelta
 
 from past.builtins import unicode
 
@@ -13,6 +13,8 @@ from apache_beam.io import WriteToText
 from  apache_beam.transforms.deduplicate import Deduplicate
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
+
+import numpy as np
 
 def select_columns(row):
     return {
@@ -64,11 +66,14 @@ def businesses_is_open_past(close_hour):
         elif row[f'close_hour_{day_name}'] < row[f'open_hour_{day_name}']:
             #if the business close the next day
             return True
+        elif row[f'close_hour_{day_name}'] == time(0,0):
+            #there are some bussiness that close at the change of day
+            return True
         else: 
             return row[f'close_hour_{day_name}'] > close_hour
 
     #use a wapper to be able to declate the hour later
-    def wapper(row):        
+    def wrap(row):        
         return is_open_past_aux(row, 'monday')\
             or is_open_past_aux(row, 'tuesday')\
             or is_open_past_aux(row, 'wednesday')\
@@ -76,11 +81,60 @@ def businesses_is_open_past(close_hour):
             or is_open_past_aux(row, 'friday')\
             or is_open_past_aux(row, 'saturday')\
             or is_open_past_aux(row, 'sunday')
-    return wapper
+    return wrap
 
+def calculate_open_and_close_time(row):
+    def calculate_open_time_day_aux(open_hour, close_hour):
+        if (close_hour is  None) or (open_hour is None):
+            return timedelta(minutes=0)
+
+        #we need a timedelta to make opetations
+        open_time = timedelta(hours=open_hour.hour,
+                              minutes=open_hour.minute)
+
+        close_time = timedelta(hours=24)\
+            if close_hour == time(0,0)\
+            else timedelta(hours=close_hour.hour,minutes=close_hour.minute)
+            
+        if close_hour < open_hour:
+            #if the business close the next day, hours like 10:00-2:00
+            return close_time + (timedelta(hours=24) - open_time)
+        else:
+            return close_time - open_time
+
+    def calculate_open_time_day(row, day_name):
+        return calculate_open_time_day_aux(row[f'open_hour_{day_name}'], row[f'close_hour_{day_name}'])
+    
+    open_time_week =  calculate_open_time_day(row, 'monday')\
+            + calculate_open_time_day(row, 'tuesday')\
+            + calculate_open_time_day(row, 'wednesday')\
+            + calculate_open_time_day(row, 'thursday')\
+            + calculate_open_time_day(row, 'friday')\
+            + calculate_open_time_day(row, 'saturday')\
+            + calculate_open_time_day(row, 'sunday')
+    row['open_time_week'] = open_time_week
+    row['close_time_week'] = timedelta(days=7) - open_time_week
+    return row
+
+def percentile(percentile):
+    def wrap(values):
+        return np.percentile(list(values) ,percentile)
+    return wrap
+
+def gen_triplet_key(row):
+    return (row['state'] + '/' + row['city'] + '/' + row['postal_code'])
+
+def join_percentiles_remove_dict(row):
+    return (
+        row[0],
+        {
+            'open_p50': row[1]['open_p50'][0],
+            'open_p95': row[1]['open_p95'][0],
+            'close_p50': row[1]['close_p50'][0],
+            'close_p95': row[1]['close_p95'][0]
+        })
 
 def run(argv=None, save_main_session=True):
-    """Main entry point; defines and runs the wordcount pipeline."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--input',
@@ -100,13 +154,43 @@ def run(argv=None, save_main_session=True):
             | 'select_columns' >> beam.Map(select_columns) # select only the fields that we need
             #| 'print_return' >> beam.Map(print_return)
             | 'parse_hours' >> beam.Map(parse_hours)
+            | 'calculate_open_and_close_time' >> beam.Map(calculate_open_and_close_time)
 
         )
-        #The number of businesses that are open past 21:00, by city and state pair.
+        ## Median and p95 opening time during the week, by postal code, city, and state triplet.
+        # Pair by city, state and postal code, for the value we only need the open time
+        businesses_open_time = (
+            businesses  
+            | 'pair_by_city_state_postal_code_open' >> beam.Map(lambda x: (gen_triplet_key(x), x['open_time_week'].total_seconds()))
+        )
+        businesses_open_time_p95 = businesses_open_time  | 'calculate_open_percentile_95' >> beam.CombinePerKey(percentile(95))
+        businesses_open_time_p50 = businesses_open_time  | 'calculate_open_percentile_50' >> beam.CombinePerKey(percentile(50))
+
+        ## Median and p95 closing time during the week, by postal code, city, and state triplet.
+        # Pair by city, state and postal code, for the value we only need the close time
+        businesses_close_time = (
+            businesses  
+            | 'pair_by_city_state_postal_code_close' >> beam.Map(lambda x: (gen_triplet_key(x), x['close_time_week'].total_seconds()))
+        )
+        businesses_close_time_p95 = businesses_close_time  | 'calculate_close_percentile_95' >> beam.CombinePerKey(percentile(95))
+        businesses_close_time_p50 = businesses_close_time  | 'calculate_close_percentile_50' >> beam.CombinePerKey(percentile(50))
+
+        #join all percentile operations
+        (
+            {'open_p50': businesses_open_time_p50,
+            'open_p95': businesses_open_time_p95,
+            'close_p50': businesses_close_time_p50,
+            'close_p95': businesses_close_time_p95} 
+                | beam.CoGroupByKey()
+                | beam.Map(join_percentiles_remove_dict)
+                | beam.Map(print)
+        )
+        ## The number of businesses that are open past 21:00, by city and state pair.
         ( businesses | 'businesses open past 21:00' >> beam.Filter(businesses_is_open_past(time(21,00)))
-            | 'pair_by_key' >> beam.Map(lambda x: ((x['state'] + '/' + x['city']), 1))
+            | 'pair_by_city_and_state' >> beam.Map(lambda x: ((x['state'] + '/' + x['city']), 1))
             | 'count_elements' >> beam.CombinePerKey(sum)
-        ) | 'print' >> beam.Map(print)
+            | 'print' >> beam.Map(print)
+        )
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
